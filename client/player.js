9a0201d2f822549ce004d7f98bbc3054f5bbee13
phile13 <47676503+phile13@users.noreply.github.com>
2023-10-26 18:51:12 -0400

    Update and rename ui.js to player.js

---
+++ client/player.js
@@ -0,0 +1,29 @@
+class player {  
+  constructor(type){
+      this.type = type;    
+      this.keycodes["Numpad7"] = "MOVE.NW";
+      this.keycodes["Numpad8"] = this.keycodes["ArrowUp"] = "MOVE.N"; 
+      this.keycodes["Numpad9"] = "MOVE.NE";
+      this.keycodes["Numpad6"] = this.keycodes["ArrowRight"] = "MOVE.E";
+      this.keycodes["Numpad3"] = "MOVE.SE"; 
+      this.keycodes["Numpad2"] = this.keycodes["ArrowDown"] = "MOVE.S";  
+      this.keycodes["Numpad1"] = "MOVE.SW"; 
+      this.keycodes["Numpad4"] = this.keycodes["ArrowLeft"] = "MOVE.W";
+
+      this.keycodes["KeyA"] = "AIM.L";
+      this.keycodes["KeyD"] = "AIM.R";
+      this.keycodes["KeyW"] = "AIM.U";
+      this.keycodes["KeyX"] = "AIM.D";
+
+      ui.keycodes["Space"] = "AIM.FIRE";
+  }
+
+  HandleKeyDown(code){
+
+  }
+  
+  HandleKeyUp(code){
+
+  }
+  
+}
