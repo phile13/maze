11d81ed79dcf2b5268516f582f281ae76248693f
phile13 <47676503+phile13@users.noreply.github.com>
2023-10-26 20:56:27 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ class server_controller{
     console.log("Leaving Constructor");
   }
 
-   NextId(){
+  NextId(){
     return this.next_client_id++;
   }
   
@@ -20,25 +20,7 @@ class server_controller{
   Connection(client){
     console.log("Connection");
     let id = this.NextId();
-    this.clients[client] = id;
-    this.clients[id] = client;
-    
-    client.on("message", (evt) => {
-      console.log("Receive");
-      let data;
-      try{
-        data = JSON.parse(JSON.parse(evt));
-        console.log(data);
-        console.log("Text");
-        if("NEW" in data){
-          client.send(`{"ID":${id}}`);
-        }
-      }
-      catch(ex){
-        console.log(ex);
-        client.send(evt);
-      }   
-    });
+    this.clients[id] = new thing(id, client);
   }
 }
 
