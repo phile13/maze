bd46bc30366d705cc81a135ee5a1f7d93f15abd6
phile13 <47676503+phile13@users.noreply.github.com>
2023-10-26 20:52:55 -0400

    Update thing.js

--- server/thing.js
+++ server/thing.js
@@ -1,4 +1,22 @@
 class thing {  
   constructor(socket){
     this.socket = socket;
+    client.on("message", (evt) => {
+      console.log("Receive");
+      let data;
+      try{
+        data = JSON.parse(JSON.parse(evt));
+        console.log(data);
+        console.log("Text");
+        if("NEW" in data){
+          client.send(`{"ID":${id}}`);
+        }
+      }
+      catch(ex){
+        console.log(ex);
+        client.send(evt);
+      }   
+    });
   }
+  
+}
