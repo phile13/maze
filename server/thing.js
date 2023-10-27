863130f7c6508e7bc43e049b1ce7bf91ccce029f
phile13 <47676503+phile13@users.noreply.github.com>
2023-10-26 20:57:35 -0400

    Update thing.js

--- server/thing.js
+++ server/thing.js
@@ -1,30 +1,28 @@
 class thing {  
   constructor(id, socket){
     this.socket = socket;
-
-    
-    client.on("message", (evt) => {
-      console.log("Receive");
-      let data;
-      try{
-        data = JSON.parse(JSON.parse(evt));
-        console.log(data);
-        console.log("Text");
-        if("NEW" in data){
-          client.send(`{"ID":${id}}`);
-        }
-      }
-      catch(ex){
-        console.log(ex);
-        client.send(evt);
-      }   
-    });
+    client.on("message", (evt) => {this.Receive(evt);});
   }
   
   NextId(){
     return this.next_client_id++;
   }
 
-  
+  Receive(msg){
+    console.log("Receive");
+    let data;
+    try{
+      data = JSON.parse(JSON.parse(evt));
+      console.log(data);
+      console.log("Text");
+      if("NEW" in data){
+        client.send(`{"ID":${id}}`);
+      }
+    }
+    catch(ex){
+      console.log(ex);
+      client.send(evt);
+    }   
+  }
   
 }
