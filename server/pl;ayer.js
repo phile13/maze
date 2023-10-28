6a07b3ebaf4338826284f37f7a08e597eac461fa
phile13 <47676503+phile13@users.noreply.github.com>
2023-10-28 15:40:06 -0400

    Create pl;ayer.js

---
+++ server/pl;ayer.js
@@ -0,0 +1,17 @@
+const ServerController = require("./server_controller.js");
+const Thing = require("./thing.js");
+
+class Player extends Thing{
+  constructor(id, socket){
+    super(id, socket);
+  }
+
+  HandleReceiveJSONObj(json){
+    ServerController.SendBinaryTo(binary, "not_in_hash" , hash);
+  }
+
+  HandleReceiveBinary(binary){
+  }
+}
+
+module.exports = WalkieTalkie;
