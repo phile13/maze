dd326775b25f7af86c1a2693f3cf77cd735e680b
phile13 <47676503+phile13@users.noreply.github.com>
2023-10-28 11:55:03 -0400

    Update server_controller.js

--- server/server_controller.js
+++ server/server_controller.js
@@ -1,1 +1,85 @@
+class server_controller{
+  static clients = {};
+  
+  constructor(){
+    this.next_client_id = 0;
 
+    this.ws = null;
+    this.server = HttpsServer({
+      cert : fs.readFileSync(config.get('cert')),
+      key: fs.readFileSync(config.get('key'))
+    });
+  
+    this.ws = new WebSocket({server: this.server});
+    this.ws.on("open", (evt) => {console.log("open");this.Open(evt)});
+    this.ws.on("connection", (evt) => {console.log("connection");this.Connection(evt)});
+
+    this.server.listen(32123, (err) =>{
+      if(err){
+        console.log(err);
+        process.exit();
+      }
+      console.log("Server is Listening");
+    });
+    console.log("Leaving Constructor");
+  }
+
+  NextId(){
+    return this.next_client_id++;
+  }
+  
+  Open(){
+    console.log("Listening");
+  }
+
+  Connection(client){
+    console.log("Connection");
+    let id = this.NextId();
+    server_controller.clients[id] = new WalkieTalkie(id, client);
+  }
+
+  static SendTextTo(msg, who, hash){
+    if(who == "everyone"){
+      for (const [id, client] of Object.entries(server_controller.clients)) {
+        client.SendText(msg);
+      }
+    }
+    else if(who == "not_in_hash"){
+      for (const [id, client] of Object.entries(server_controller.clients)) {
+        if(!(hash[id])){
+          client.SendText(msg);
+        }
+      }
+    }
+    else if(who == "in_hash"){
+      for (const [id, client] of Object.entries(server_controller.clients)) {
+        if(!(hash[id])){
+          client.SendText(msg);
+        }
+      }
+    }
+  }
+
+  static SendBinaryTo(msg, who, hash){
+    if(who == "everyone"){
+      for (const [id, client] of Object.entries(server_controller.clients)) {
+        client.SendBinary(msg);
+      }
+    }
+    else if(who == "not_in_hash"){
+      for (const [id, client] of Object.entries(server_controller.clients)) {
+        if(!(hash[id])){
+          client.SendBinary(msg);
+        }
+      }
+    }
+    else if(who == "in_hash"){
+      for (const [id, client] of Object.entries(server_controller.clients)) {
+        if(!(hash[id])){
+          client.SendBinary(msg);
+        }
+      }
+    }
+  }
+  
+}
