05f5c05cca4dbe9e9f545b811a28a887bc6724aa
phile13 <47676503+phile13@users.noreply.github.com>
2023-10-28 15:52:24 -0400

    Update pl;ayer.js

--- server/pl;ayer.js
+++ server/pl;ayer.js
@@ -4,12 +4,51 @@ const Thing = require("./thing.js");
 class Player extends Thing{
   constructor(id, socket){
     super(id, socket);
+    this.tool = null;
   }
 
   HandleReceiveJSONObj(json){
-    ServerController.SendBinaryTo(binary, "not_in_hash" , hash);
+    if("TYPE" in json && "ACTION" in json){
+      switch(json.TYPE){
+        case "MOVE":
+          this.TryToMove(json.ACTION);
+          break;
+        case "PICKUP":
+          this.TryToPickup(json.ACTION);
+          break;
+        case "PUTDOWN":
+          this.TryToPutDown(json.ACTION);
+          break;
+        case "USE":
+          this.TryToUse(json.ACTION);
+          break;
+      }
+    }
+    ServerController.SendBinaryTo(json, "everyone");
   }
 
+  TryToMove(action){
+    
+  }
+
+  TryToPickup(action){
+    if(this.tool == null){
+      //look in gamespace for nearby item
+    }
+  }
+
+  TryToPutDown(action){
+    if(this.tool != null){
+      //look in gamespace for place to put item
+    }
+  }
+
+  TryToUse(action){
+    if(this.tool != null){
+      this.tool.Use(action);
+    }
+  }
+  
   HandleReceiveBinary(binary){
   }
 }
