7e1aeeda68026486dff9f1c2ce1a508f6d522e39
phile13 <47676503+phile13@users.noreply.github.com>
2023-10-28 17:30:03 -0400

    Update game_space.js

--- server/game_space.js
+++ server/game_space.js
@@ -3,29 +3,69 @@ class GameSpace{
     this.rows = rows;
     this.cols = cols;
     this.board = {};
+    this.accessible_types = {FLAT:true};
+    this.inaccessible_types = {WALL:true};
+
+    let max_r = this.rows-1;
+    let max_c = this.cols-1;
     for(let r = 0; r < this.rows; r++){
       this.board[r] = {};
       for(let c = 0; c < this.cols; c++){
-        this.board[r][c] = {type:"FLAT",thing:null,tool:null};
+        this.board[r][c] = {type:(r==0||c==0||r==max_r||c==max_c)?"FLAT":"WALL",thing:null,tool:null};
       }
     }
   }
   
   CheckIfSpaceIsOpen(x,y){
     if(y in this.board && x in this.board[y]){
-      let space = this.board[x][y];
+      let space = this.board[y][x];
       if(space.type in this.accessible_types && space.thing == null && space.tool == null){
-        
+        return true;
       }
     }
+    return false;
   }
 
-  PickupNearbyTool(x,y){
-    return this.gs.PickupNearbyTool(x,y);
+  PickupNearbyTool(id){
+    if(id in this.things){
+      let thing = this.things[id];
+      if(thing.tool == null){
+        let x = thing.x;
+        let y = thing.y;
+        
+        for(let r = y-1; r < y+2; r++){
+          for(let c = x-1; c < x+2; c++){
+            let space = this.board[r][c];
+            if(space.tool){
+              thing.tool = space.tool;
+              this.board[r][c].tool = null;
+            }
+          }
+        }
+      }
+    }
+    return null;
   }
 
-  FindSpotToPutTool(x,y){
-    return this.gs.FindSpotToPutTool(x,y);
+  FindSpotToPutTool(id){
+    if(id in this.things){
+      let thing = this.things[id];
+      if(thing.tool != null){
+        let x = thing.x;
+        let y = thing.y;
+        
+        for(let r = y-1; r < y+2; r++){
+          for(let c = x-1; c < x+2; c++){
+            let space = this.board[r][c];
+            if(space.type in this.accessible_types && space.thing == null && space.tool == null){
+              thing.tool = null;
+              this.board[r][c].tool = space.tool;
+            }
+          }
+        }
+      }
+    }
+    return null;
   }
 
   ServerController.UseTool(x, y, tool){
