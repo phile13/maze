99083c80e7eb573397327cee9f666c7404e80ee0
phile13 <47676503+phile13@users.noreply.github.com>
2023-10-28 16:17:43 -0400

    Update player.js

--- server/player.js
+++ server/player.js
@@ -5,6 +5,7 @@ class Player extends Thing{
   constructor(id, socket){
     super(id, socket);
     this.tool = null;
+    this.dirs_map = {N:[0,-1], NE:[1,-1], E:[1,0], SE:[1,1], S:[0,1], SW:[-1,1], W:[-1,0], NW:[-1,-1] };
   }
 
   HandleReceiveJSONObj(json){
@@ -28,18 +29,38 @@ class Player extends Thing{
   }
 
   TryToMove(action){
-    
+    if(action in this.dirs_map){
+      let dir = this.dirs_map[action];
+      if(ServerController.CheckIfSpaceIsOpen(this.x + dir[0], this.y + dir[1])){
+        this.x += dir[0];
+        this.y += dir[1];
+        this.heading = action;
+        ServerController.SendTextTo(`{"ID":${this.id},"X":${this.x},"Y":${this.y},"HEADING":"${action}","TOOL":"${(this.tool)?this.tool.id:""}","HEALTH":${this.health}}`, "everyone");
+      }
+    }
   }
 
   TryToPickup(action){
     if(this.tool == null){
-      //look in gamespace for nearby item
+      this.tool = ServerController.PickupNearbyTool(this.x, this.y);
+      if(this.tool != null){
+        this.tool.x = this.x;
+        this.tool.y = this.y;
+        this.tool.owned = true;
+        ServerController.SendTextTo(`{"ID":${this.tool.id},"OWNED":"true"}`, "everyone");
+      }
     }
   }
 
   TryToPutDown(action){
     if(this.tool != null){
-      //look in gamespace for place to put item
+      let loc = ServerController.FindSpotToPutTool(this.x, this.y);
+      if(loc.length == 2){
+        this.tool.x = loc[0];
+        this.tool.y = loc[1];
+        this.tool.owned = false;
+        ServerController.SendTextTo(`{"ID":${this.tool.id},"X":${this.tool.x},"Y":${this.tool.y},"OWNED":"false"}`, "everyone");
+      }
     }
   }
 
