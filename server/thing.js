e17acd070fc0cc4a15213f4bda004351ad4ba2cc
phile13 <47676503+phile13@users.noreply.github.com>
2023-10-28 16:18:05 -0400

    Update thing.js

--- server/thing.js
+++ server/thing.js
@@ -5,6 +5,7 @@ class Thing {
     this.x = 0;
     this.y = 0;
     this.heading = "S";
+    this.health = 100;
     
     this.socket = socket;
     this.socket.on("message", (evt) => {this.Receive(evt);});
