c4a3d9c7f62cb5d7b065ec7a199a439eef5604a2
phile13 <47676503+phile13@users.noreply.github.com>
2023-10-28 16:25:37 -0400

    Update tool.js

--- server/tool.js
+++ server/tool.js
@@ -1,1 +1,31 @@
+class Tool extends Thing {  
+  constructor(id, socket){
+    this.id = id;
+    this.type = "THING";
+    this.x = 0;
+    this.y = 0;
+    this.heading = "S";
+    this.health = 100;
+    
+    this.socket = socket;
+    this.socket.on("message", (evt) => {this.Receive(evt);});
+  }
+  
+  SendText(msg){
+    this.socket.send(msg);
+  }
 
+  SendBinary(msg){
+    this.socket.send(msg);
+  }
+
+  HandleReceiveJSONObj(json){
+
+  }
+
+  HandleReceiveBinary(binary){
+
+  }
+}
+
+module.exports = Tool;
