56353657328dc5be3520ddf405287703195725be
phile13 <47676503+phile13@users.noreply.github.com>
2023-11-04 17:32:18 -0400

    Update server_controller.js

--- server/server_controller.js
+++ server/server_controller.js
@@ -31,7 +31,7 @@ class ServerController{
     console.log("Leaving Constructor");
 
     const GameSpace = require('./game_space');
-    this.gs = new GameSpace(33,33);
+    this.gs = new GameSpace(17,17);
 
     ServerController.sc = this;
   }
