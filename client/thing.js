64dddf6c8ff5be21e50bfc29bc2732c92fb851ac
phile13 <47676503+phile13@users.noreply.github.com>
2023-11-05 12:14:46 -0500

    Update thing.js

--- client/thing.js
+++ client/thing.js
@@ -11,6 +11,7 @@ class thing {
     document.addEventListener('keyup',(evt) => {this.OnKeyUpHandler(evt)});
     console.log("walkie talkie created");
     this.gs = null;
+    this.spritesheet = null;
   }
 
   Open(){
