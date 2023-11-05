5d16d2db225a2ff6b8e4b56715bee512653f260e
phile13 <47676503+phile13@users.noreply.github.com>
2023-11-05 11:03:05 -0500

    Update gamespace.js

--- client/gamespace.js
+++ client/gamespace.js
@@ -1,8 +1,10 @@
 class gamespace{
   constructor(board){
     this.board = board;
+    this.app = new PIXI.Application({ background: '#1099bb', view: document.getElementById("board") });
+    document.body.appendChild(this.app.view);
+    
     this.background = new PIXI.Graphics();
-
     this.colors = ["grey","blue"];
     for (let row = 0; row < num_rows; row++) {
         for (let col = 0; col < num_cols; col++) {
@@ -11,6 +13,6 @@ class gamespace{
             this.background.endFill();
         }
     }
-    
+    this.app.stage.addChild(this.background);
   }
 }
