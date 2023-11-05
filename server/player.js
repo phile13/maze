f3c290c376570f80c6be0b9b011736ed5a0d7fbe
phile13 <47676503+phile13@users.noreply.github.com>
2023-11-05 16:21:37 -0500

    Update player.js

--- server/player.js
+++ server/player.js
@@ -9,7 +9,7 @@ class Player extends Thing{
   }
 
   HandleReceiveJSONObj(json){
-    console.log("HandleReceiveJSONObj.Player");
+    //console.log("HandleReceiveJSONObj.Player");
     if("TYPE" in json && "ACTION" in json){
       switch(json.TYPE){
         case "MOVE":
