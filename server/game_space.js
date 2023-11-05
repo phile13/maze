ae77ead3e42e612191691a10aa6530cd6457e5c8
phile13 <47676503+phile13@users.noreply.github.com>
2023-11-05 15:39:35 -0500

    Update game_space.js

--- server/game_space.js
+++ server/game_space.js
@@ -17,9 +17,10 @@ class GameSpace{
         this.board[r][c] = {type:(r%4==0||c%4==0)?"WALL":"FLOOR",thing:null,tool:null};
       }
     }
+    this.WilsonsMazeAlgorithm();
     //this.CreateMaze(this.rows-1,0,this.cols-1,0,"H");
     this.json_board = JSON.stringify(this.BoardObject());
-    //console.log(JSON.stringify(this.BoardObject()));
+    console.log(JSON.stringify(this.BoardObject()));
   }
 
   RegisterThing(thing){
@@ -159,7 +160,7 @@ class GameSpace{
     return rows;
   }
 
-  WilsonsMazeAlgorithm(width, height){
+  WilsonsMazeAlgorithm(){
     let ids_list = [];
     let hash = {}
     let count = 0;
