e35573ce5b12a338217bfa07692ad3484acb1548
phile13 <47676503+phile13@users.noreply.github.com>
2023-11-10 16:29:35 -0500

    Update gamespace.js

--- client/gamespace.js
+++ client/gamespace.js
@@ -12,7 +12,7 @@ class gamespace{
     this.height = this.board.length;
     this.width = this.board[0].length;
     this.myscale = 2;
-    this.centerY =  this.app.* this.myscale/2;
+    this.centerY =  this.app.stage.height * this.myscale/2;
     this.centerX = this.width * this.myscale/2;
 
     this.offsetR = -this.height / 2;
