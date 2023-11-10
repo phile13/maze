d895d6d29725b066d6c850fc8e94a768eb49dec9
phile13 <47676503+phile13@users.noreply.github.com>
2023-11-10 16:52:59 -0500

    Update gamespace.js

--- client/gamespace.js
+++ client/gamespace.js
@@ -3,6 +3,9 @@ class gamespace{
     this.board = board;
     this.app = new PIXI.Application({ background: '#1099bb', view: document.getElementById("board") , resize: window });
     document.body.appendChild(this.app.view);
+
+    this.app.stage.scale.x = this.app.stage.scale.y = 4;
+    
     this.others = {};
     this.background = new PIXI.Graphics();
     this.colors = ["lightgrey","darkgrey"];
@@ -11,7 +14,7 @@ class gamespace{
     
     this.height = this.board.length;
     this.width = this.board[0].length;
-    this.myscale = 4;
+    
     this.centerX = (this.canvasW/2);
     this.centerY = (this.canvasH/2);
 
@@ -21,25 +24,18 @@ class gamespace{
         let row = this.board[r];
         for (let c = 0, C = this.offsetC; c < this.width; c++, C++) {
             this.background.beginFill((R == 0 || C == 0) ? 'red' : this.colors[row[c]]);
-            this.background.drawRect(C*this.myscale, R*this.myscale, this.myscale, this.myscale);
+            this.background.drawRect(C*this.app.stage.scale.x, R*this.app.stage.scale.y, this.app.stage.scale.x, this.app.stage.scale.y);
             this.background.endFill();
         }
     }
     this.app.stage.addChild(this.background);
-    this.app.stage.scale.x = this.app.stage.scale.y = 1;
     
+    this.app.stage.rotation = 0.78539816339;
     
     this.myid = id;
     this.mytype = type;
     this.me = this.CreateThings(id, type , true);
-    this.me.x = (x+this.canvasW/2)*this.myscale*this.app.stage.scale.x;
-    this.me.y = (y+this.canvasH/2)*this.myscale*this.app.stage.scale.y;
-  
-    // // PI / 4
-    this.app.stage.x = this.centerX;
-    this.app.stage.y = this.centerY;
-    this.app.stage.rotation = 0.78539816339;
-    //this.RotateStage(this.me.x, this.me.y);
+    this.move(this.me,msg.X,msg.Y);
   }
 
   CreateThings(id, type, is_me = false){
@@ -50,7 +46,7 @@ class gamespace{
     else{
       g.beginFill("red");
     }
-    g.drawRect(0, 0, this.myscale, this.myscale);
+    g.drawRect(0, 0, this.app.stage.scale.x, this.app.stage.scale.y);
     g.endFill();
     this.app.stage.addChild(g);
     this.others[id] = g;
@@ -65,11 +61,11 @@ class gamespace{
   }
 
   move(who,x,y){
-    who.x = x *this.myscale*this.app.stage.scale.x;
-    who.y = y *this.myscale*this.app.stage.scale.y;
-    if(msg.ID == this.myid){
-      this.app.stage.x = -who.x*+this.centerX;
-      this.app.stage.y = -who.y*+this.centerY;
+    who.x = x *this.app.stage.scale.x;
+    who.y = y *this.app.stage.scale.y;
+    if(who.id == this.myid){
+      this.app.stage.x = -who.x+this.centerX;
+      this.app.stage.y = -who.y+this.centerY;
     }
   }
 
@@ -79,12 +75,7 @@ class gamespace{
       this.CreateThings(msg.ID, msg.THING);
       who = this.others[msg.ID];
     }
-    who.x = msg.X*this.myscale*this.app.stage.scale.x;
-    who.y = msg.Y*this.myscale*this.app.stage.scale.y;
-    if(msg.ID == this.myid){
-      this.app.stage.x = -who.x*+this.centerX;
-      this.app.stage.y = -who.y*+this.centerY;
-    }
+    this.move(who,msg.X,msg.Y);
   }
   
 }
