63a3f4c0be10efc6ca873c6844f89ad9a4022b1f
phile13 <47676503+phile13@users.noreply.github.com>
2023-11-11 21:12:00 -0500

    Update gamespace.js

--- client/gamespace.js
+++ client/gamespace.js
@@ -4,7 +4,7 @@ class gamespace{
     this.app = new PIXI.Application({ background: '#1099bb', view: document.getElementById("board") , resize: window });
     document.body.appendChild(this.app.view);
 
-    this.myscale = 4;
+    this.myscale = 1;
     this.app.stage.scale.x = this.app.stage.scale.y = 1;
     
     this.others = {};
