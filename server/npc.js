3274c8b64b3f7938973c7cdc057ecdfc6909aba8
phile13 <47676503+phile13@users.noreply.github.com>
2023-11-12 10:58:08 -0500

    Update npc.js

--- server/npc.js
+++ server/npc.js
@@ -7,10 +7,23 @@ class NPC {
     if(this.type in this){
       this[this.type]();
     }
+    this.x = 0;
+    this.y = 0;
+    this.heading = 'S';
+    this.tool = null;
+    this.health = 100;
+    this.speed = 1000;
   }
 
+  send_message(){
+    ServerController.SendTextTo(`{"ID":${this.id},"TYPE":"MOVE","THING":"${this.type}","X":${this.x},"Y":${this.y},"HEADING":"${json.ACTION}","TOOL":"${(this.tool)?this.tool.id:""}","HEALTH":${this.health}}`, "everyone", {});
+  }
+  
   zombie(){
     
+    setInterval(()=>{
+      
+    }, this.speed);
   }
   
 }
