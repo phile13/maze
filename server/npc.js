88bba5931d4bcb3bb7b146b3ddb69940efbaf3ff
phile13 <47676503+phile13@users.noreply.github.com>
2023-11-12 10:52:50 -0500

    Update npc.js

--- server/npc.js
+++ server/npc.js
@@ -1,5 +1,16 @@
+const ServerController = require("./server_controller.js");
+
 class NPC {  
   constructor(type){
     this.id = let id = ServerController.NextId();
+    this.type = type;
+    if(this.type in this){
+      this[this.type]();
+    }
   }
+
+  zombie(){
+    
+  }
+  
 }
