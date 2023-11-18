5acd804ea5895a84e3197adda2d09098eab742ec
phile13 <47676503+phile13@users.noreply.github.com>
2023-11-18 16:53:18 -0500

    Update gamespace.js

--- client/gamespace.js
+++ client/gamespace.js
@@ -83,8 +83,8 @@ export class gamespace{
       who.graphic.position.z = msg.Y;
       if(msg.ID == this.myid){
         this.camera.position.x = msg.X;
-        this.camera.position.z = msg.Y + 5;
-        this.camera.position.y = 7;
+        this.camera.position.z = msg.Y + 3;
+        this.camera.position.y = 3;
         this.camera.lookAt(msg.X , 0, msg.Y);
         //if("HEADING" in msg){
           //let dir = this.heading_angles[msg.HEADING];
