c32220f154ef4b8b7ceb58c933b848ee44d0e173
phile13 <47676503+phile13@users.noreply.github.com>
2023-11-18 16:43:31 -0500

    Update gamespace.js

--- client/gamespace.js
+++ client/gamespace.js
@@ -9,7 +9,7 @@ export class gamespace{
  
     //create game stage
     this.scene = new THREE.Scene();
-    this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    this.camera = new THREE.PerspectiveCamera( 150, window.innerWidth / window.innerHeight, 0.1, 1000 );
     
     this.renderer = new THREE.WebGLRenderer();
     this.renderer.setSize( window.innerWidth, window.innerHeight );
